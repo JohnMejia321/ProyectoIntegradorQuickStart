20590f8162d27619bf3c296ab88662547aa52d96
JohnMejia <johnmejia@quind.io>
2021-12-14 11:45:46 -0500

    se crea formulario de pago

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
 
-  constructor(
-    private cartService: CartService
-  ) { }
+constructor(
+  private cartService: CartService,
+  private formBuilder: FormBuilder,
+) {}
 }
